e48488106a4b714578cc3c4533b2e06f9a9eed24
sandeepLabs-42 <sandeep.p@project42labs.com>
2022-12-25 15:46:01 +0530

    user-status code updated

--- template/src/models/user/user-status.model.mjs
+++ template/src/models/user/user-status.model.mjs
@@ -1,30 +1,44 @@
 import uStatus from "./user-status.postgres.mjs";
 
-
-const createUstatus= async (data,transaction)=>{
-    return await uStatus.create(data,{transaction})
-}
-
+const createUstatus = async (data, transaction) => {
+  return await uStatus.create(data, { transaction });
+};
 
 const findAllUstatus = async (transaction) => {
   return await uStatus.findAll({ transaction });
 };
 
+// const updateUstatus = async (uStatusId, data, transaction) => {
+//   return await uStatus.update(
+//         data,
+//         {
+//           where: {
+//             id: uStatusId,
+//           },
+//         },
+//         { transaction }
+//       );
+//     };
 
 const updateUstatus = async (uStatusId, data, transaction) => {
-  return await uStatus.update(
-        data,
-        {
-          where: {
-            id: uStatusId,
-          },
-        },
-        { transaction }
-      );
-    };
+  let findData = await uStatus.findOne(
+    { where: { id: uStatusId } },
+    transaction
+  );
+  if (findData) {
+    if ("name" in data) {
+      findData.name = data.name;
+    }
+    if ("color" in data) {
+      findData.color = data.color;
+    }
+    return findData;
+  }
+  return false;
+};
 
 const deleteUstatus = async (uStatusId, transaction) => {
-   // console.log(ticketId)
+  // console.log(ticketId)
   await uStatus.destroy({
     where: {
       id: uStatusId,
@@ -32,4 +46,4 @@ const deleteUstatus = async (uStatusId, transaction) => {
   });
 };
 
-export { createUstatus, findAllUstatus,updateUstatus,deleteUstatus};
+export { createUstatus, findAllUstatus, updateUstatus, deleteUstatus };
